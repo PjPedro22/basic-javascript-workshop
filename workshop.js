6a8d6d401ad1ee5a148512d854d9e7345b4cf7fe
Guillaume Legare <legare.guillaume.1990@gmail.com>
2017-07-03 23:53:18 +0000

    impletment addTwoNumbers question

--- workshop.js
+++ workshop.js
@@ -27,8 +27,13 @@ function letterAtPosition(inputString, position) {
 }
 
 function addTwoNumbers(num1, num2) {
-
-}
+   if (typeof num1 === 'number' && typeof num2 === 'number') {
+         return num1 + num2;
+     }
+     else {
+         return undefined;
+     }
+    }
 
 function multiplyTwoNumbers(num1, num2) {
 
